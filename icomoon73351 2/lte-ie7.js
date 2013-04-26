b36fceb6f9138b28e5e1b57b205f52882a8984bd
Graphic Design <graphicdesign@Graphic-Design-2.local>
2013-04-26 10:00:11 -0700

    icomoon typeface

---
+++ icomoon73351 2/lte-ie7.js
@@ -0,0 +1,37 @@
+/* Load this script using conditional IE comments if you need to support IE 7 and IE 6. */
+
+window.onload = function() {
+	function addIcon(el, entity) {
+		var html = el.innerHTML;
+		el.innerHTML = '<span style="font-family: \'icomoon\'">' + entity + '</span>' + html;
+	}
+	var icons = {
+			'icon-P6-Icon-Fonts-12' : '&#x61;',
+			'icon-P6-Icon-Fonts-11' : '&#x73;',
+			'icon-P6-Icon-Fonts-10' : '&#x64;',
+			'icon-P6-Icon-Fonts-09' : '&#x66;',
+			'icon-P6-Icon-Fonts-08' : '&#x67;',
+			'icon-P6-Icon-Fonts-07' : '&#x68;',
+			'icon-P6-Icon-Fonts-06' : '&#x6a;',
+			'icon-P6-Icon-Fonts-05' : '&#x6b;',
+			'icon-P6-Icon-Fonts-03' : '&#x6c;',
+			'icon-P6-Icon-Fonts-04' : '&#x7a;'
+		},
+		els = document.getElementsByTagName('*'),
+		i, attr, html, c, el;
+	for (i = 0; ; i += 1) {
+		el = els[i];
+		if(!el) {
+			break;
+		}
+		attr = el.getAttribute('data-icon');
+		if (attr) {
+			addIcon(el, attr);
+		}
+		c = el.className;
+		c = c.match(/icon-[^\s'"]+/);
+		if (c && icons[c[0]]) {
+			addIcon(el, icons[c[0]]);
+		}
+	}
+};
